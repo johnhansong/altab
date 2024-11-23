29d20c3f1a7d02148560d742263eb15f92c839a6
Johann <johnhansong@gmail.com>
2024-11-23 15:11:24 -0800

    tags model

---
+++ app/models/tag.py
@@ -0,0 +1,23 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+class Tag(db.Model):
+  __tablename__ = "tags"
+
+  if environment == "production":
+    __table_args__ = {"schema": SCHEMA}
+
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String, nullable=False)
+  created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+  updated_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+  #relationships
+  
+
+  def to_dict(self):
+    return {
+      'id': self.id,
+      'name': self.name,
+      'created_at': self.created_at,
+      'updated_at': self.updated_at
+    }
